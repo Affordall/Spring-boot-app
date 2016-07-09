8db8fb0a3db6c1810be949eb34323109ec60bd9c
Vladimir Degtev <->
2016-07-09 12:31:21 +0300

    Initial commit

---
+++ src/main/java/com/realjamapps/Application.java
@@ -0,0 +1,16 @@
+package com.realjamapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by affy on 08.07.16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
